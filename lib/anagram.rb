271e7416c18af23860aaa0ca97f42dedf6f50e4e
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-21 15:30:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram 
+  
+  attr_accessor 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array)
+    array.select do |x| 
+      (@word.split("").sort) == (x.split("").sort)
+    end 
+  end 
+  
+  
+end 
